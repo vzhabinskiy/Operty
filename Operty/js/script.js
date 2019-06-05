fce6481e9904a9726d92b67fb4e9d0149f4cfac1
Zhabinskiy Ivan <39421451+vzhabinskiy@users.noreply.github.com>
2019-06-05 19:57:15 +0300

    Add files via upload

--- Operty/js/script.js
+++ Operty/js/script.js
@@ -1,3 +1,18 @@
-$(".select").select2({
+$(".select-projects").select2({
     minimumResultsForSearch: Infinity
+});
+
+$(".select-roles").select2({
+    minimumResultsForSearch: Infinity
+});
+
+var slider = document.getElementById('slider');
+
+noUiSlider.create(slider, {
+    start: [20, 80],
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 100
+    }
 });
