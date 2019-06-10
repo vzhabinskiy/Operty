ae77aa55ba942f97ebfe3a8d637c973159b3a3ac
Zhabinskiy Ivan <39421451+vzhabinskiy@users.noreply.github.com>
2019-06-10 04:33:23 +0300

    Add files via upload

--- Operty/js/script.js
+++ Operty/js/script.js
@@ -6,13 +6,31 @@ $(".select-roles").select2({
     minimumResultsForSearch: Infinity
 });
 
-var slider = document.getElementById('slider');
-
-noUiSlider.create(slider, {
-    start: [20, 80],
-    connect: true,
-    range: {
-        'min': 0,
-        'max': 100
-    }
+$(".select-region").select2({
+    minimumResultsForSearch: Infinity
 });
+$(".select-executors").select2({
+    minimumResultsForSearch: Infinity
+});
+
+let slider = document.getElementById("range-slider");
+    noUiSlider.create(slider, {
+        start: [9, 65],
+        connect: true,
+        step: 1,
+        range: {
+            'min': 9,
+            'max': 65
+        },
+    format: wNumb({
+            decimals: 0,
+            thousand: ' ',
+        })
+    });
+let SliderValues = [
+        document.getElementById('start'),
+        document.getElementById('end')
+    ];
+    slider.noUiSlider.on('update', function(values, handle) {
+        SliderValues[handle].innerHTML = values[handle];
+    });
