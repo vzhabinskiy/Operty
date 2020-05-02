cb82a1c9679d7cef8a6df5240a548641066eb08e
Zhabinskiy Ivan <39421451+vzhabinskiy@users.noreply.github.com>
2020-05-02 22:55:08 +0300

    Delete menu.js

--- Operty/js/menu.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    let menuMobile=$('.mobile-menu');
-    let menu=$('.menu_mob');
-    menuMobile.click(function(){
-        menuMobile.toggleClass('mobile-menu_active');
-        menu.toggleClass('menu_active');
-    });
-
-});
